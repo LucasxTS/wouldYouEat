2e25f3ea1c605fa0be02877d8fa9beb8baa24e38
Lucas <lucastorres6431@gmail.com>
2023-08-19 14:30:34 -0300

    created layout

--- app/src/main/java/com/example/wouldyoueat/MainActivity.kt
+++ app/src/main/java/com/example/wouldyoueat/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.wouldyoueat
 
+import android.graphics.LinearGradient
+import android.graphics.Shader
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.wouldyoueat.databinding.ActivityMainBinding
@@ -10,5 +12,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        textDegradeColor()
+    }
+
+    private fun textDegradeColor() {
+        val appTitle = binding.appTitle
+        val appTitleText = "Fruits"
+        val colors = intArrayOf(resources.getColor(R.color.yellow),resources.getColor(R.color.orange), resources.getColor(R.color.red))
+        val shader = LinearGradient(
+            0f, 0f, 0f, appTitle.textSize,
+            colors,
+            null,
+            Shader.TileMode.CLAMP
+        )
+        appTitle.text = appTitleText
+        appTitle.paint.shader = shader
     }
 }
